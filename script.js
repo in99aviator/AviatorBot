0afb5847d79b805b50b80ddd42b4c8ec48a624ea
in99aviator <in99soft.dev.pvt.ltd@gmail.com>
2025-04-10 01:56:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
-// Firebase config (from Brijesh bhai)
-const firebaseConfig = {
-  apiKey: "AIzaSyAETHe_FcNSrHUAqeIgGlLchjhEWI44u0k",
-  authDomain: "aviatorbot-bcbdd.firebaseapp.com",
-  databaseURL: "https://aviatorbot-bcbdd-default-rtdb.firebaseio.com",
-  projectId: "aviatorbot-bcbdd",
-  storageBucket: "aviatorbot-bcbdd.firebasestorage.app",
-  messagingSenderId: "116364075167",
-  appId: "1:116364075167:web:6be73837f5b8710678c2aa",
-  measurementId: "G-QRK3TD2CJW"
-};
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+  import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+  const firebaseConfig = {
+    apiKey: "AIzaSyAETHe_FcNSrHUAqeIgGlLchjhEWI44u0k",
+    authDomain: "aviatorbot-bcbdd.firebaseapp.com",
+    databaseURL: "https://aviatorbot-bcbdd-default-rtdb.firebaseio.com",
+    projectId: "aviatorbot-bcbdd",
+    storageBucket: "aviatorbot-bcbdd.appspot.com", // FIXED here
+    messagingSenderId: "116364075167",
+    appId: "1:116364075167:web:6be73837f5b8710678c2aa",
+    measurementId: "G-QRK3TD2CJW"
+  };
 
-// Login logic
-function login() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const auth = getAuth(app);
 
-  auth.signInWithEmailAndPassword(email, password)
-    .then(() => {
-      document.getElementById("login-screen").style.display = "none";
-      document.getElementById("splash-screen").style.display = "block";
+  document.getElementById("loginBtn").addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-      setTimeout(() => {
-        document.getElementById("splash-screen").style.display = "none";
-        document.getElementById("home-screen").style.display = "block";
-      }, 4000);
-    })
-    .catch((error) => {
-      document.getElementById("login-error").innerText = error.message;
-    });
-}
-
-// Random number generator
-function generateRandom() {
-  const value = (Math.random() * 10).toFixed(2) + "x";
-  document.getElementById("multiplier").innerText = value;
-}
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        alert("Login Success!");
+        window.location.href = "home.html";
+      })
+      .catch((error) => {
+        alert("Firebase Error: " + error.message);
+      });
+  });
+</script>
