0cf85542abe6334c2d28793d97c5fdff046c9331
in99aviator <in99soft.dev.pvt.ltd@gmail.com>
2025-04-10 02:36:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
-  import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyAETHe_FcNSrHUAqeIgGlLchjhEWI44u0k",
-    authDomain: "aviatorbot-bcbdd.firebaseapp.com",
-    databaseURL: "https://aviatorbot-bcbdd-default-rtdb.firebaseio.com",
-    projectId: "aviatorbot-bcbdd",
-    storageBucket: "aviatorbot-bcbdd.appspot.com", // FIXED here
-    messagingSenderId: "116364075167",
-    appId: "1:116364075167:web:6be73837f5b8710678c2aa",
-    measurementId: "G-QRK3TD2CJW"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const auth = getAuth(app);
-
-  document.getElementById("loginBtn").addEventListener("click", () => {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        alert("Login Success!");
-        window.location.href = "home.html";
-      })
-      .catch((error) => {
-        alert("Firebase Error: " + error.message);
-      });
-  });
-</script>
+function toggleButton(button) {
+  if (button.innerText.includes("CASH OUT")) {
+    button.innerHTML = "BET NOW";
+    button.style.backgroundColor = "#00ff66";
+    button.style.color = "black";
+  } else {
+    button.innerHTML = "CASH OUT<br><span>1514.00 INR</span>";
+    button.style.backgroundColor = "orange";
+    button.style.color = "black";
+  }
+}
